5b2ddb314e7d744f55127459d1f661180b457ebd
esmailkazemi <smk182018@gmail.com>
2019-12-05 18:32:07 +0330

    add doQuery

--- core/model.php
+++ core/model.php
@@ -32,6 +32,18 @@ class Model
         }
         return $result;
     }
+    function doQuery($sql, $values = [])
+    {
+
+        $stmt = self::$conn->prepare($sql);
+
+        foreach ($values as $key => $value) {
+
+            $stmt->bindValue($key + 1, $value);
+
+        }
+        $stmt->execute();
+    }
 
     public static function generateHash($password)
     {
