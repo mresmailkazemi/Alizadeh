157953e810f4d5a3a6277a5148e880af6c168c6c
esmailkazemi <smk182018@gmail.com>
2019-12-05 19:46:11 +0330

    fix sendsms

--- models/model_addMember.php
+++ models/model_addMember.php
@@ -163,6 +163,21 @@ class model_addMember extends model
     {
 
 
+        $username = "faraz09196145343";
+        $password = '0371477905';
+        $from = "+983000505";
+        $pattern_code = "oxwi8vg0t5";
+        $to = array($mobile);
+        $input_data = array("name" => "");
+        $url = "https://ippanel.com/patterns/pattern?username=" . $username . "&password=" . urlencode($password) . "&from=$from&to=" . json_encode($to) . "&input_data=" . urlencode(json_encode($input_data)) . "&pattern_code=$pattern_code";
+        $handler = curl_init($url);
+        curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $input_data);
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($handler);
+        echo $response;
+
+
         $this->updateCounterSms(sizeof($mobile));
     }
 }
